d413460e2c4fe334dbc96716a35fa2e53016f224
oz.mendelsohn <oz.mendelsohn@gmail.com>
2021-04-14 15:30:04 +0300

    update to support "pip install ."

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='pbs-utils',
+    name='pbs_utils',
     version='0.1',
     description='Package for submitting and running PBS jobs by using Python API',
-    py_moddules=['pbs-utils'],
-    package_dir={'': 'src'},
-    install_requires=['petname'],
+    py_modules=["pbs_utils"],
+    package_dir={"": "src"},
+    install_requires=['petname', 'pandas'],
     author='Oz Mendelsohn',
     author_email='ozyosef.mendelsohn@weizmann.ac.il',
     license='LICENSE.txt',
